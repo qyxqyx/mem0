28f19144c3c20bf5efa24b929e7f89188768cba7
qyx <qinyunxiao@cuc.edu.cn>
2025-03-30 00:03:11 +0000

    123

--- mem0/__init__.py
+++ mem0/__init__.py
@@ -4,3 +4,9 @@ __version__ = importlib.metadata.version("mem0ai")
 
 from mem0.client.main import MemoryClient, AsyncMemoryClient  # noqa
 from mem0.memory.main import Memory  # noqa
+
+
+
+print(123)
+
+print(123)
